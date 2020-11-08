879f384577d32c122aaf82bba8616c236b3347e3
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2020-11-08 22:07:40 +0100

    Fetch contentful graphql API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const query = `
+query {
+  person(id:"15jwOBqpxqSAOy2eOO4S0m") {
+    name
+    shortBio
+  }
+  productCollection (where: { price_lte: 40 }) {
+    total
+    items {
+      commonName
+      price
+    }
+  }
+}
+`
+
 function App() {
+  fetch(`https://graphql.contentful.com/content/v1/spaces/u7a44gonzwk8/?access_token=P3vWnkLS98iN1fzTqXRfE_7sh4EKgeOpHJ5arTIQK5Y`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ query })
+  })
+  .then(response => response.json())
+  .then(json => console.log(json.data));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React + contentful (Delivery API)- Graphql</h1>
     </div>
   );
 }
